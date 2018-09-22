215df348f580a6e2f0cac7f4d6b315f7f23fcd51
Dhanvi Desai <dhanvidesai99@gmail.com>
2018-09-23 00:22:07 +0530

    Added Get Method

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos',(req,res) => {
   });
 });
 
+app.get('/todos',(req,res) => {
+  Todo.find().then((todos) => {
+    res.status(200).send({todos});
+  },(err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000 , ()=> {
   console.log('Server up and running in port 3000');
 });
